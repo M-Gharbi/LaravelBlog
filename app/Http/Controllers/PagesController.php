36ff2bd8098a3a38123101c2ad7eba549d16b49b
Mohammed-Gharbi-hup <mohammed.gharbi@outlook.com>
2021-01-25 13:05:47 +0300

    adding Front-end pages

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class pagesController extends Controller
+{
+    public function getIndex() {
+        $first= "Mohammed ";
+        $last= "Alshammari ";
+
+        $fullname = $first."".$last;
+        $email="mohammed.gharbi@outlook.com";
+        $data['email']=$email;
+        $data['fullname']=$fullname;        
+
+        return view('index')->withData($data);
+    }
+
+    public function getAbout() {
+        $first= "Mohammed ";
+        $last= "Alshammari";
+
+        $fullname = $first."".$last;
+        $email="mohammed.gharbi@outlook.com";
+        $data['email']=$email;
+        $data['fullname']=$fullname;        
+
+        return view('about')->withData($data);
+    }
+
+    public function getContact() {       
+        return view('contact');
+    }
+
+}
